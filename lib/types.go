ed1e004f1abd116667e1232c7cdb75eb1892c3bb
macinnir <rob.macinnis@gmail.com>
2018-11-18 22:07:47 -0500

    Config types

--- lib/types.go
+++ lib/types.go
@@ -37,8 +37,8 @@ type ChangeFile struct {
 // Config contains a set of configuration values used throughout the application
 type Config struct {
 	ChangeSetPath string `toml:"changesetPath"`
-
-	DatabaseType string `toml:"databaseType"`
+	DatabaseType  string `toml:"databaseType"`
+	BasePackage   string `toml:"basePackage"`
 
 	Connection struct {
 		Host         string `toml:"host"`
@@ -48,10 +48,10 @@ type Config struct {
 	}
 
 	Packages struct {
-		Cache  string `toml: "cache"`
+		Cache  string `toml:"cache"`
 		Models string `toml:"models"`
-		Schema string `toml: "schema"`
-		Repos  string `tomls: "repos"`
+		Schema string `toml:"schema"`
+		Repos  string `tomls:"repos"`
 	}
 
 	Dirs struct {
