9aec3148d58cbcaf3b2362da9df9ca5d34462c59
macinnir <rob.macinnis@gmail.com>
2021-07-04 18:19:34 -0400

    Database interface

---
+++ core/lib/utils/db/IDB.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+)
+
+// IDB is a container for db interactions
+type IDB interface {
+	Close()
+	NamedExec(query string, arg interface{}) (sql.Result, error)
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Get(dest interface{}, query string, args ...interface{}) error
+	Select(dest interface{}, query string, args ...interface{}) error
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
+	ExecMany(stmts []string, chunkSize int) (e error)
+}
