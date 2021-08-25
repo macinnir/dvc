e3081eeeb70d4a2392f99359e4f348eebebf908f
macinnir <rob.macinnis@gmail.com>
2021-08-25 19:02:28 -0400

    Changed accountID to account on IUserContainer; Added comments

--- core/lib/utils/types/IUserContainer.go
+++ core/lib/utils/types/IUserContainer.go
@@ -1,10 +1,16 @@
 package types
 
 type IUserContainer interface {
+	// ID is the unique identifier for the user
 	ID() int64
-	AccountID() int64
+	// Account returns the AccountID for the user
+	Account() int64
+	// Activated returns whether or not the user is activated
 	Activated() bool
+	// Disabled returns whether or not the user is disabled
 	Disabled() bool
+	// Locked returns whether or not th user is locked
 	Locked() bool
+	// Permissions returns a slice of permission names
 	Permissions() []string
 }
