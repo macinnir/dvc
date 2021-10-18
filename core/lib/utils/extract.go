d369774f86b0ac1549f5ac1f8c949d84a11427c5
macinnir <rob.macinnis@gmail.com>
2021-10-18 15:08:14 -0400

    BatchIDs and extractIDs

---
+++ core/lib/utils/extract.go
@@ -0,0 +1,55 @@
+package utils
+
+func BatchIDs(ids []int64, batchSize int) [][]int64 {
+
+	batchCount := len(ids) / batchSize
+	batchRemainder := len(ids) % batchSize
+	if batchRemainder > 0 {
+		batchCount++
+	}
+
+	batches := make([][]int64, batchCount)
+
+	for k := range batches {
+
+		batchLen := batchSize
+
+		// Is Last?
+		if k == batchCount-1 && batchRemainder > 0 {
+			batchLen = batchRemainder
+		}
+
+		batches[k] = make([]int64, batchLen)
+
+		l := 0
+		indexOfID := k * batchSize
+		finish := indexOfID + batchLen
+
+		for {
+			if indexOfID == finish {
+				break
+			}
+
+			batches[k][l] = ids[indexOfID]
+
+			l++
+			indexOfID++
+		}
+	}
+
+	return batches
+}
+
+func ExtractIDs(objLen int, fn func(index int) int64) []int64 {
+
+	ids := []int64{}
+	gate := map[int64]struct{}{}
+	for k := 0; k < objLen; k++ {
+		if _, ok := gate[fn(k)]; !ok {
+			gate[fn(k)] = struct{}{}
+			ids = append(ids, fn(k))
+		}
+	}
+
+	return ids
+}
