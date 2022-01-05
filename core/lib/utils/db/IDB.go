ee67c03d326e811e92976cd2e7626d3de3d9c900
macinnir <rob.macinnis@gmail.com>
2022-01-05 12:20:53 -0500

    Added comments to IDB

--- core/lib/utils/db/IDB.go
+++ core/lib/utils/db/IDB.go
@@ -14,6 +14,6 @@ type IDB interface {
 	Select(dest interface{}, query string, args ...interface{}) error
 	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
 	ExecMany(stmts []string, chunkSize int) (e error)
-	Host() string
-	Name() string
+	Host() string // The host name (from config)
+	Name() string // The name of the database (from config)
 }
